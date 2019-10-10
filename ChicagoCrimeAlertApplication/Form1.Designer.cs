2bcf73bcb70b87c601e670a1d360c04d796005d7
Vismark Juarez <vismarkjuarez@gmailcom>
2019-10-10 00:06:43 -0500

    Added temporary text boxes for entering user-specific information -- namely, phone number and text message.

--- ChicagoCrimeAlertApplication/Form1.Designer.cs
+++ ChicagoCrimeAlertApplication/Form1.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.customMessageLabel = new System.Windows.Forms.Label();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.customMessageTextBox = new System.Windows.Forms.TextBox();
+            this.sendMessageButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Location = new System.Drawing.Point(12, 20);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(81, 13);
+            this.phoneNumberLabel.TabIndex = 0;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            this.phoneNumberLabel.Click += new System.EventHandler(this.PhoneNumberLabel_Click);
+            // 
+            // customMessageLabel
+            // 
+            this.customMessageLabel.AutoSize = true;
+            this.customMessageLabel.Location = new System.Drawing.Point(12, 61);
+            this.customMessageLabel.Name = "customMessageLabel";
+            this.customMessageLabel.Size = new System.Drawing.Size(91, 13);
+            this.customMessageLabel.TabIndex = 1;
+            this.customMessageLabel.Text = "Custom Message:";
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(109, 20);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(118, 20);
+            this.phoneNumberTextBox.TabIndex = 2;
+            // 
+            // customMessageTextBox
+            // 
+            this.customMessageTextBox.Location = new System.Drawing.Point(109, 58);
+            this.customMessageTextBox.Multiline = true;
+            this.customMessageTextBox.Name = "customMessageTextBox";
+            this.customMessageTextBox.Size = new System.Drawing.Size(202, 97);
+            this.customMessageTextBox.TabIndex = 3;
+            // 
+            // sendMessageButton
+            // 
+            this.sendMessageButton.Location = new System.Drawing.Point(172, 174);
+            this.sendMessageButton.Name = "sendMessageButton";
+            this.sendMessageButton.Size = new System.Drawing.Size(75, 23);
+            this.sendMessageButton.TabIndex = 4;
+            this.sendMessageButton.Text = "SEND";
+            this.sendMessageButton.UseVisualStyleBackColor = true;
+            this.sendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sendMessageButton);
+            this.Controls.Add(this.customMessageTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.customMessageLabel);
+            this.Controls.Add(this.phoneNumberLabel);
             this.Name = "mainForm";
             this.Text = "Chicago Crime Alert Application";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label customMessageLabel;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox customMessageTextBox;
+        private System.Windows.Forms.Button sendMessageButton;
     }
 }
 
--- ChicagoCrimeAlertApplication/Form1.cs
+++ ChicagoCrimeAlertApplication/Form1.cs
@@ -16,5 +16,28 @@ namespace ChicagoCrimeAlertApplication
         {
             InitializeComponent();
         }
+
+        private void PhoneNumberLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            /*
+           TODO:
+               - Input vlidation (phone number cannot be < || > 10 digits).
+               - Phone number input should only be numbers.
+               - The custom message and phone number cannot be null.
+            */
+
+            //Store the phone numbere entered by the user
+            String phoneNumber = phoneNumberTextBox.Text;
+            Console.WriteLine("The user entered the phone number: " + phoneNumber);
+
+            //Store the custom message entered by the user.
+            String customMessage = customMessageTextBox.Text;
+            Console.WriteLine("The user entered the following custom message: " + customMessage);
+        }
     }
 }
