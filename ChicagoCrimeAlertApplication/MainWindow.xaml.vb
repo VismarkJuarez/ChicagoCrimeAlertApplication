83782ab68e2c95ac420d1bc66987d379afcfe802
jack <jbroncato@luc.edu>
2019-10-23 17:21:53 -0500

    Wpf for frontend, new branch

---
+++ ChicagoCrimeAlertApplication/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub Home_Click(sender As Object, e As RoutedEventArgs)
+        Home.Visibility = System.Windows.Visibility.Visible
+        Stats.Visibility = System.Windows.Visibility.Hidden
+    End Sub
+
+    Private Sub StatsBtn_Click(sender As Object, e As RoutedEventArgs) Handles StatsBtn.Click
+        Home.Visibility = System.Windows.Visibility.Hidden
+        Stats.Visibility = System.Windows.Visibility.Visible
+    End Sub
+End Class
