8d6ac63a8ea5836b180faca59ad0dd25bc9f5006
jack <jbroncato@luc.edu>
2019-10-30 19:57:01 -0500

    update to GUI

--- ChicagoCrimeAlertApplication/MainWindow.xaml.vb
+++ ChicagoCrimeAlertApplication/MainWindow.xaml.vb
@@ -2,10 +2,40 @@
     Private Sub Home_Click(sender As Object, e As RoutedEventArgs)
         Home.Visibility = System.Windows.Visibility.Visible
         Stats.Visibility = System.Windows.Visibility.Hidden
+        About.Visibility = System.Windows.Visibility.Hidden
+        News.Visibility = System.Windows.Visibility.Hidden
+        Alerts.Visibility = System.Windows.Visibility.Hidden
     End Sub
 
     Private Sub StatsBtn_Click(sender As Object, e As RoutedEventArgs) Handles StatsBtn.Click
         Home.Visibility = System.Windows.Visibility.Hidden
         Stats.Visibility = System.Windows.Visibility.Visible
+        About.Visibility = System.Windows.Visibility.Hidden
+        News.Visibility = System.Windows.Visibility.Hidden
+        Alerts.Visibility = System.Windows.Visibility.Hidden
+    End Sub
+
+    Private Sub CrimeNewsBtn_Click(sender As Object, e As RoutedEventArgs)
+        Home.Visibility = System.Windows.Visibility.Hidden
+        Stats.Visibility = System.Windows.Visibility.Hidden
+        About.Visibility = System.Windows.Visibility.Hidden
+        News.Visibility = System.Windows.Visibility.Visible
+        Alerts.Visibility = System.Windows.Visibility.Hidden
+    End Sub
+
+    Private Sub AlertBtn_Click(sender As Object, e As RoutedEventArgs) Handles AlertBtn.Click
+        Home.Visibility = System.Windows.Visibility.Hidden
+        Stats.Visibility = System.Windows.Visibility.Hidden
+        About.Visibility = System.Windows.Visibility.Hidden
+        News.Visibility = System.Windows.Visibility.Hidden
+        Alerts.Visibility = System.Windows.Visibility.Visible
+    End Sub
+
+    Private Sub AboutBtn_Click(sender As Object, e As RoutedEventArgs) Handles AboutBtn.Click
+        Home.Visibility = System.Windows.Visibility.Hidden
+        Stats.Visibility = System.Windows.Visibility.Hidden
+        About.Visibility = System.Windows.Visibility.Visible
+        News.Visibility = System.Windows.Visibility.Hidden
+        Alerts.Visibility = System.Windows.Visibility.Hidden
     End Sub
 End Class
